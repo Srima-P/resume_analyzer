aa87f625f147873f57933b4e77ec8b4df9637bbd
Srima-P <srimaperiyasamy01@gmail.com>
2026-04-07 18:02:27 +0530

    first commit

--- backend/main.py
+++ backend/main.py
@@ -167,3 +167,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
